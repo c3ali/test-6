e8b52c046472a52b410ba1b2bf7fbf603421cb8f
c3ali <72300663+c3ali@users.noreply.github.com>
2025-11-11 08:13:28 +0100

    Add models/__init__.py

---
+++ models/__init__.py
@@ -0,0 +1,22 @@
+from .user import User
+from .board import Board
+from .list import List
+from .card import Card
+from .comment import Comment
+from .label import Label
+from .association_tables import (
+    board_members,
+    card_labels,
+    card_assignees
+)
+__all__ = [
+    "User",
+    "Board",
+    "List",
+    "Card",
+    "Comment",
+    "Label",
+    "board_members",
+    "card_labels",
+    "card_assignees"
+]
